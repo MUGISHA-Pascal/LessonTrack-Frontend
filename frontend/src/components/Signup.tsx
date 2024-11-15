f9fa4374f5e0de94f9c835f18fc675e1e3a96763
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-11-15 15:10:50 +0200

    frontend

---
+++ frontend/src/components/Signup.tsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirm] = useState("");
+  return (
+    <div>
+      <main className="flex items-center justify-center min-h-screen">
+        <section className="bg-white shadow-md rounded-lg p-8 max-w-md w-full">
+          <h1 className="text-2xl font-semibold text-center text-gray-700 mb-6">
+            Signup for Connectify
+          </h1>
+
+          <form
+            className="space-y-6"
+            //   onSubmit={handlesubmit}
+          >
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                name="username"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+              <p id="usernameError" className="text-blue-400 font-thin"></p>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email Address
+              </label>
+              <input
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                type="email"
+                id="email"
+                name="email"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+            <p id="emailError" className="text-blue-400 font-thin"></p>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                name="password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+              <p id="passwordError" className="text-blue-400 font-thin"></p>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirm_password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirm_password"
+                name="confirm_password"
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirm(e.target.value);
+                }}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:opacity-90"
+            >
+              Signup
+            </button>
+          </form>
+
+          <div className="text-center mt-4">
+            <p className="text-gray-600">
+              Already have an account?
+              <a
+                href="/auth/login"
+                className="text-blue-500 ml-2 hover:underline"
+              >
+                Login
+              </a>
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Signup;
